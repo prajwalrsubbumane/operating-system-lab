56b237154a8ab5bab0834e6e343ceea79e006851
prajwalrsubbumane <135595114+prajwalrsubbumane@users.noreply.github.com>
2023-07-10 09:53:33 +0000

    roundr

--- roundr.c
+++ roundr.c
@@ -66,3 +66,34 @@ printf("\n Average Turn Around Time: \t%f", avg_wt);
 printf("\n Average Waiting Time: \t%f", avg_tat);  
 
 }  
+/*
+Total number of process in the system: 4
+
+ Enter the Arrival and Burst time of the Process[1]
+ Arrival time is:       2
+ 
+Burst time is:  1
+
+ Enter the Arrival and Burst time of the Process[2]
+ Arrival time is:       1
+ 
+Burst time is:  5
+
+ Enter the Arrival and Burst time of the Process[3]
+ Arrival time is:       4
+ 
+Burst time is:  1
+
+ Enter the Arrival and Burst time of the Process[4]
+ Arrival time is:       0
+ 
+Burst time is:  6
+Enter the Time Quantum for the process:         2
+
+ Process No              Burst Time              TAT             Waiting Time 
+Process No[1]            1                               -1                      -2
+Process No[3]            1                               2                       1
+Process No[2]            5                               8                       3
+Process No[4]            6                               13                      7
+ Average Turn Around Time:      2.250000
+ Average Waiting Time:*/
